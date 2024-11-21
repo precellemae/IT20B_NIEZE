315228d3a6f7f1aa0293dbd065d75ea00865a0d4
precellemae <precellemaenieze@gmail.com>
2024-11-21 12:44:58 +0800

    Lab activity

---
+++ linkedlistexample/linkedlistexample.java
@@ -0,0 +1,30 @@
+
+package ayoon_linkedlist;
+
+import java.util.LinkedList;
+public class ayoon_LinkedList {
+
+    public static void main(String[] args) {
+        LinkedList<String> creatures = new LinkedList<>();
+        
+        creatures.add("Dragon");
+        creatures.addFirst("Wyvern");
+        creatures.addLast("Tikbalang");
+        creatures.add(1, "Goblin");
+        
+        
+        System.out.println("Linked List Original: " + creatures);
+        System.out.println(creatures.size());
+        
+        if (creatures.contains("Dragon")) {
+            System.out.println("Hatdog");
+        } else {
+            System.out.println("False");
+        }
+
+        boolean containsDragon = creatures.contains("Lion");
+        System.out.println(containsDragon);
+
+    }
+    
+}
